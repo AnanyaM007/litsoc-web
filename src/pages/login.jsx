dca05db283f5d97269d7975da39ccc28ab0367e0
Ananya <99496164+AnanyaM007@users.noreply.github.com>
2023-06-03 16:50:07 +0530

    build issues fixed

---
+++ src/pages/login.jsx
@@ -0,0 +1,12 @@
+import { Button, Link, Typography, Stack, Box } from "@mui/material";
+
+const Login = () => {
+  return (
+    <>
+   <Typography>Login</Typography>
+      </>
+  );
+}
+
+export default Login;
+
