38f51b24023e22317f6c2fb24df28efe04bf15c3
Ananya <99496164+AnanyaM007@users.noreply.github.com>
2023-06-03 15:40:42 +0530

    Navbar Dropdown fixed

--- components/Landing/WordofWeek.jsx
+++ components/Landing/WordofWeek.jsx
@@ -5,10 +5,10 @@ const WordofWeek = () => {
         <>
             <Stack flexDirection={"row"} flexWrap={"wrap"} padding={"80px 50px"} justifyContent={"space-evenly"}>
                 <Stack maxWidth={"800px"}>
-                    <Typography variant="h4" sx={{ marginBottom: "30px" }}>Word of the Week</Typography>
-                    <Typography variant="h6">Words are the roots that hold the tree of thoughts of the writer firmly to the soil of the reader&apos;s mind. To get the reader connected to the roots more thoroughly, Our Team brings you a new word every week in the form of the &quot;WORD OF THE WEEK&quot;.</Typography>
+                    <Typography sx={{ marginBottom: "30px",fontSize:{xs:"30px",sm:"35px"} }}>Word of the Week</Typography>
+                    <Typography sx={{fontSize:{xs:"18px",sm:"22px"}}}>Words are the roots that hold the tree of thoughts of the writer firmly to the soil of the reader&apos;s mind. To get the reader connected to the roots more thoroughly, Our Team brings you a new word every week in the form of the &quot;WORD OF THE WEEK&quot;.</Typography>
                 </Stack>
-                <Box component={"img"} src={"Literature-rafiki.png"} width={{ xs: "250px", sm: "300px", md: "380px" }} />
+                <Box component={"img"} src={"Literature-rafiki.png"} width={{ xs: "250px", sm: "300px", md: "380px" }} marginTop={{xs:"50px", sm:"50px", md:"0px"}} />
             </Stack>
         </>
     );
