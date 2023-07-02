98e3ba2065b629cffb177d024fc0761e26039075
Ananya <99496164+AnanyaM007@users.noreply.github.com>
2023-07-02 23:08:25 +0530

    build

--- src/pages/versse.jsx
+++ src/pages/versse.jsx
@@ -2,7 +2,7 @@ import { Button, Link, Typography, Stack, Box } from "@mui/material";
 import Navbar from "../../components/Common/Navbar/Navbar";
 import SecondNavbar from "../../components/Common/Navbar/SecondNavbar";
 import Footer from "../../components/Common/Footer/Footer";
-import VersseBody from "../../components/Versse/Verssebody";
+import VersseBody from "../components/Versse/Verssebody";
 
 const Versse = () => {
     return (
