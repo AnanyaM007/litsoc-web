813191d1aff1fe91d6b6beaaa52c248560b47dbd
Ananya <99496164+AnanyaM007@users.noreply.github.com>
2023-07-02 21:55:42 +0530

    links added

--- components/Landing/ScribeScreen.jsx
+++ components/Landing/ScribeScreen.jsx
@@ -4,10 +4,21 @@ const ScribeScreen = () => {
     return (
         <>
             <Stack flexDirection={"row"} flexWrap={"wrap-reverse"} padding={"20px 50px"} justifyContent={"space-evenly"}>
-                <Box component={"img"} src={"Imagination-bro.png"} width={{ xs: "250px", sm: "300px", md: "380px" }} marginTop={{xs:"50px", sm:"50px", md:"0px"}} />
+                <Box component={"img"} src={"Imagination-bro.png"} width={{ xs: "250px", sm: "300px", md: "380px" }} marginTop={{ xs: "50px", sm: "50px", md: "0px" }} />
                 <Stack maxWidth={"800px"}>
-                <Typography sx={{ marginBottom: "30px",fontSize:{xs:"30px",sm:"35px"} }}>Scribe Screen</Typography>
-                <Typography sx={{fontSize:{xs:"18px",sm:"22px"}}}>Welcome to Scribe Screen, a digital sanctuary where writers share their creative works in the form of poetry, stories, extracts, and articles. Immerse yourself in the enchantment of words and embark on a journey of imagination and inspiration.</Typography>
+                    <Link href="/scribescreen">
+                        <Typography
+                            sx={{
+                                marginBottom: "30px",
+                                fontSize: { xs: "30px", sm: "35px" },
+                                color: "#000",
+                                '&:hover': {
+                                    color: "#f07e05",
+                                }
+                            }}>Scribe Screen
+                        </Typography>
+                    </Link>
+                    <Typography sx={{ fontSize: { xs: "18px", sm: "22px" } }}>Welcome to Scribe Screen, a digital sanctuary where writers share their creative works in the form of poetry, stories, extracts, and articles. Immerse yourself in the enchantment of words and embark on a journey of imagination and inspiration.</Typography>
                 </Stack>
             </Stack>
         </>
